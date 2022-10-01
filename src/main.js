5db53673ff381158938b5bb3a77a644277127c82
(Messi) Junlin Wu <tallmessiwu@qq.com>
2022-10-01 02:39:55 -0700

    :heavy_plus_sign: 全局配置了axios并增加了依赖。

--- src/main.js
+++ src/main.js
@@ -2,12 +2,16 @@ import {createApp} from 'vue'
 import App from './app.vue'
 import './index.less'
 import {Lazyload} from "vant"
-import router from "./router/router";
+import router from "./router/router"
+import axios from "axios"
 
 const app = createApp(App)
 
-app.use(Lazyload)
+axios.defaults.baseURL = "http://localhost:667"
+app.provide("axios", axios)
 
+// 使用懒加载
+app.use(Lazyload)
 // 挂载路由模块
 app.use(router)
 
