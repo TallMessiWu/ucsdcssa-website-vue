5c68fa3a286f1422215e99cdacd41def5b2dbcc1
Jason011125 <zic022@ucsd.edu>
2023-02-17 14:34:17 -0800

    :bulb: 增加了注释与改进了代码样式

--- src/components/home-headline.vue
+++ src/components/home-headline.vue
@@ -5,50 +5,56 @@
       <el-main class="main">
         <el-row>
           <el-col :span="22" :offset="2">
+<!--            大标题-->
             <div class="headline">公益服务，以人为本</div>
           </el-col>
         </el-row>
 
         <el-row>
           <el-col :span="22" :offset="2">
+<!--            副标题-->
             <div class="headline-subtext">我们用爱发电，照亮您留学的路途</div>
           </el-col>
         </el-row>
 
         <el-row>
           <el-col :span="3.5" :offset="2">
+<!--            热门资讯按钮-->
             <div class="headline-button">
               <el-button link size="large" class="headline-button-text" :icon="Right" round>近期热门资讯</el-button>
             </div>
           </el-col>
           <el-col :span="3.5">
             <div class="headline-button">
+              <!--            关注公众号按钮-->
               <el-button link size="large" class="headline-button-text" :icon="Right" round>关注CSSA公众号</el-button>
             </div>
           </el-col>
         </el-row>
-
+<!--        4张卡片-->
         <el-row class="cards">
           <el-col :span="6.5" :offset="2">
+<!--            标题卡片-->
             <el-card id="header-card">
               <div class="card1-header">
+<!--                标题卡片的标题-->
                 <text class="bold">我们是...</text>
               </div>
 
               <br>
-
+<!--              标题卡片正文-->
               <div class="card1-text">
                 <text class="bold">中国学生学者联合会</text>（Chinese Students and Scholars Association, 简称CSSA）
               </div>
 
               <br>
-
+<!--              标题卡片正文-->
               <div class="card1-text">
                 是海外中国学生、学者的全球官方组织，活跃在各大洲的高等学府
               </div>
 
               <br>
-
+<!--              标题卡片正文-->
               <div class="card1-text">
                 <text class="bold">UCSD-CSSA</text>作为全球CSSA大家庭中的一员，成立于1985年，旨在从学术、职业发展、生活、文娱等全方面服务UCSD华人学生、学者
               </div>
@@ -56,6 +62,7 @@
           </el-col>
 
           <el-col :span="5">
+<!--            子卡片容器-->
             <el-card class="children-card-container">
               <div class="children-card">
                 <div class="icon">
@@ -64,16 +71,18 @@
                   </el-icon>
                 </div>
                 <br>
+<!--                子卡片标题-->
                 <div class="card-header">
                   <span>校园资讯</span>
                 </div>
 
                 <br>
-
+<!--                子卡片正文-->
                 <div class="card-text">
                   选课，求职，交学费，网课签证，专业介绍，新冠检测...
                 </div>
                 <br>
+<!--                子卡片正文-->
                 <div class="card-text">
                   关注CSSA公众号让您获得第一手校内外信息
                 </div>
@@ -91,19 +100,19 @@
                   </el-icon>
                 </div>
                 <br>
-
+<!--                子卡片标题-->
                 <div class="card-header">
                   <span>活动组织</span>
                 </div>
 
                 <br>
-
+<!--                子卡片正文-->
                 <div class="card-text">
                   申研分享，春晚，狼人杀，德扑，桌游夜，故事汇，茶话会，篮球赛...
                 </div>
 
                 <br>
-
+<!--                子卡片正文-->
                 <div class="card-text">
                   学术生活娱乐我们全安排！
                 </div>
@@ -121,13 +130,16 @@
 
                 </div>
                 <br>
+<!--                子卡片标题-->
                 <div class="card-header">
                   <span>新生服务</span>
                 </div>
                 <br>
+<!--                子卡片正文-->
                 <div class="card-text">新生群，国内暑假见面会，机场接机，手机银行卡办理...
                 </div>
                 <br>
+<!--                子卡片正文-->
                 <div class="card-text">
                   初到异国他乡，CSSA与你一起共度难关，适应新环境
                 </div>
@@ -158,31 +170,16 @@ export default {
 
 <style scoped lang="less">
 
-  .bold {
-    font-weight: bold;
-  }
+  .header-image-container {
+  background-color: white;
+  background-image: url("../assets/geisel_library.jpeg");
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-position: center center;
+}
 
   .main {
-    overflow: visible;
-  }
-
-  .header-image-container {
-    background-color: white;
-    background-image: url("../assets/geisel_library.jpeg");
-    background-size: cover;
-    background-repeat: no-repeat;
-    background-position: center center;
-  }
-
-  .white-background {
-    height: 10vw;
-    width: 100%;
-    background-color: white;
-  }
-
-  .cards {
-    position: relative;
-    top: 8vw;
+  overflow: visible;
   }
 
   .headline {
@@ -222,9 +219,13 @@ export default {
     background-color: #982f2d;
   }
 
+  .cards {
+    position: relative;
+    top: 8vw;
+  }
 
-  .icon {
-    color: #982f2d;
+  .el-card {
+    margin-top: 5vw;
   }
 
   #header-card {
@@ -235,6 +236,27 @@ export default {
     background-color: #982f2d;
   }
 
+  .card1-header {
+    color: white;
+    font-size: xx-large;
+    margin: 1vw;
+  }
+
+  .bold {
+    font-weight: bold;
+  }
+
+  .card1-text {
+    margin: 1vw;
+    color: white;
+  }
+
+  .white-background {
+    height: 10vw;
+    width: 100%;
+    background-color: white;
+  }
+
   .children-card-container {
     border-radius: 1vw;
     margin-left: 1.5vw;
@@ -242,7 +264,6 @@ export default {
   }
 
   .children-card {
-
     display: flex;
     flex-direction: column;
     justify-content: center;
@@ -250,8 +271,8 @@ export default {
     text-align: center;
   }
 
-  .el-card {
-    margin-top: 5vw;
+  .icon {
+    color: #982f2d;
   }
 
   .card-header {
@@ -259,14 +280,4 @@ export default {
     font-weight: bold;
   }
 
-  .card1-header {
-    color: white;
-    font-size: xx-large;
-    margin: 1vw;
-  }
-
-  .card1-text {
-    margin: 1vw;
-    color: white;
-  }
 </style>
