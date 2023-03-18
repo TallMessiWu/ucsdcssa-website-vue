2cd3a688b5c8eda67db774ae033150f59c9e7fa4
JohnChen034 <jic034@ucsd.edu>
2023-03-18 11:40:40 -0700

    :memo: 更新了一城一程新生手册以及新生大礼包

--- src/components/the-header.vue
+++ src/components/the-header.vue
@@ -53,10 +53,10 @@
                 <template #dropdown>
                   <el-dropdown-menu>
                     <!--pdf文件太大了，打包的时候不会被编译成代码。因此需要放在public文件夹里面。这样打包的时候会被直接复制到dist文件夹中。-->
-                    <a href="/一城一程——UCSD%20CSSA新生手册%204.0.pdf" target="_blank">
+                    <a href="/一城一程——UCSD%20CSSA新生手册%205.0.pdf" target="_blank">
                       <el-dropdown-item>"一城一程"UCSD新生手册</el-dropdown-item>
                     </a>
-                    <a href="https://mp.weixin.qq.com/s/wn6jQs_kf8qFkqiISoEZRA" target="_blank">
+                    <a href="https://mp.weixin.qq.com/s/sFuOISwjORAB46fc_CrCeQ" target="_blank">
                       <el-dropdown-item>新生资源</el-dropdown-item>
                     </a>
                     <a href="https://mp.weixin.qq.com/s/nYxpPqBgKjTkYPmhf7qJMg" target="_blank">
